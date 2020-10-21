7ea62f4f69bf52d78a428796d8281f2d94f23c38
Nick Repin <65354374+igotbitches@users.noreply.github.com>
2020-10-21 16:16:59 +0300

    Add files via upload

--- Course 1. White belt/Week 1/equation.cpp
+++ Course 1. White belt/Week 1/equation.cpp
@@ -1,21 +1,35 @@
 #include <iostream>
 #include <cmath>
+
 using namespace std;
 
 int main(){
-	int a, b, c;
+	double a, b, c;
 	cin >> a >> b >> c;
-	int discr = pow(b, 2) - 4 * a * c;
 	
-	if (discr < 0){
+	if (a != 0){
+		double discr = pow(b, 2) - 4 * a * c;
+		
+		if (discr < 0){
 		cout << " ";
+		} 
+		else if (discr >= 0) {
+			double x1 = (-b + (sqrt(discr))) / (2 * a);
+			double x2 = (-b - (sqrt(discr))) / (2 * a);
+			
+			if (x1 == x2){
+				cout << x1;
+			}
+			else{
+				cout << x1 << " " << x2;
+			}
+		}
 	}
-	else if (discr == 0){
-		cout << -b / (2 * a);
-	}
-	else {
-		cout << (-b + (sqrt(discr))) / (2 * a) << " " << (-b - (sqrt(discr))) / (2 * a);
+	else if (b != 0){
+		cout << -1 * c / b;
 	}
 	
 	return 0;
 }
+
+
